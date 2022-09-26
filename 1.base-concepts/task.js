3b7680f79b608c953dba417b04d8747977e33e07
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2022-09-26 22:28:37 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-b - Math.sqrt(discriminant) )/(2*a));
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr =[];
+  } 
   return arr; // array
 }
 
